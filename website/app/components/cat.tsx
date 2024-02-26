02d266c79ce922f90610a50e1b2d19a084197a7c
kayYZ1 <overek7133@gmail.com>
2024-02-26 14:28:30 +0100

    fix: change image type to png

--- website/app/components/cat.tsx
+++ website/app/components/cat.tsx
@@ -1,11 +1,9 @@
-import { Box, Image } from "@chakra-ui/react"
+import { Center, Image } from "@chakra-ui/react"
 
 export default function CatPicture() {
   return (
-    <div>
-      <Box boxSize="sm">
-        <Image src={`/pixel-cat.jpg`} alt="cat" />
-      </Box>
-    </div>
+    <Center>
+      <Image src={`/pixel-cat.png`} alt="cat" w="60" h="60" />
+    </Center>
   )
 }
