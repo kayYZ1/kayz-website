8dcb0b82ae26a19f65407222e764ce5e122892a4
kayYZ1 <overek7133@gmail.com>
2024-02-26 22:18:54 +0100

    fix: fix interface export

--- website/app/components/projects.tsx
+++ website/app/components/projects.tsx
@@ -3,7 +3,7 @@
 import { Heading } from "@chakra-ui/react";
 import CardComponent from "./card";
 
-interface IProjectInfo {
+export interface IProjectInfo {
   id: number;
   title: string;
   description: string;
